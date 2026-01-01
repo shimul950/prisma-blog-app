c983a787b8dee83ac0d3b2f4a0d692b8591d0ae2
Sayedul Islam <shimulmazumder950@gmail.com>
2026-01-01 20:19:30 +0600

    added some authentication, schema

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
 import express from "express";
 import { postRouter } from "./modules/post/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.APP_URL || "http://localhost:3000",
+    credentials: true
+}))
+
+app.all('/api/auth/*splat', toNodeHandler(auth));
+
 app.use(express.json())
 
-app.use("/posts",postRouter )
+app.use("/posts", postRouter)
 
-app.get("/",(req, res)=>{
+app.get("/", (req, res) => {
     res.send('hellow world')
 })
 export default app;
